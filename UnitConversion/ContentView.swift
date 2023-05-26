0d7433de45c2c874e5fb8c499e6d6b4cc85bae18
RishavG96 <gupta.rishav2608@gmail.com>
2023-05-26 09:29:53 +0530

    Initial Commit

---
+++ UnitConversion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitConversion
+//
+//  Created by Rishav Gupta on 26/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
