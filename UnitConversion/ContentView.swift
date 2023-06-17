99bee2de6bbefe495dc6d1e55a9c2cd7c90a30aa
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-06-17 09:26:13 +0530

    Unit Conversions
    Time to Time conversion in Swift UI

--- UnitConversion/ContentView.swift
+++ UnitConversion/ContentView.swift
@@ -8,9 +8,120 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputUnit: String = "seconds"
+    @State private var outputUnit: String = "minutes"
+    @State private var timeEntered: Double = 0.0
+    @FocusState private var timeIsFocused: Bool
+    
+    private let timeMetrics = ["seconds", "minutes", "hours", "days"]
+    
+    var convertedTimeIs: Double {
+        print("value is \(timeEntered)")
+        if timeEntered == 0 {
+            return 0
+        }
+        if inputUnit == "seconds" {
+            switch outputUnit {
+            case "seconds":
+                return timeEntered
+            case "minutes":
+                return timeEntered / 60
+            case "hours":
+                return timeEntered / (60 * 60)
+            case "days":
+                return timeEntered / (60 * 60 * 24)
+            default:
+                break
+            }
+        } else if inputUnit == "minutes" {
+            switch outputUnit {
+            case "seconds":
+                return timeEntered * 60
+            case "minutes":
+                return timeEntered
+            case "hours":
+                return timeEntered / 60
+            case "days":
+                return timeEntered / (60 * 24)
+            default:
+                break
+            }
+        } else if inputUnit == "hours" {
+            switch outputUnit {
+            case "seconds":
+                return timeEntered * 60 * 60
+            case "minutes":
+                return timeEntered * 60
+            case "hours":
+                return timeEntered
+            case "days":
+                return timeEntered / 24
+            default:
+                break
+            }
+        } else if inputUnit == "days" {
+            switch outputUnit {
+            case "seconds":
+                return timeEntered * 60 * 60 * 24
+            case "minutes":
+                return timeEntered * 60 * 24
+            case "hours":
+                return timeEntered * 24
+            case "days":
+                return timeEntered
+            default:
+                break
+            }
+        }
+        
+        return 0
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            Section {
+                Picker("Select Input Unit", selection: $inputUnit) {
+                    ForEach(timeMetrics, id: \.self) {
+                        Text($0)
+                    }
+                }
+                .pickerStyle(.segmented)
+            } header: {
+                Text("Select Input Parameter")
+            }
+            
+            Section {
+                Picker("Select Output Unit", selection: $outputUnit) {
+                    ForEach(timeMetrics, id: \.self) {
+                        Text($0)
+                    }
+                }
+                .pickerStyle(.segmented)
+            } header: {
+                Text("Select Output Parameter")
+            }
+            
+            Section {
+                TextField("Enter time in \(inputUnit)", value: $timeEntered, format: .number)
+                    .keyboardType(.numberPad)
+                    .focused($timeIsFocused)
+            } header: {
+                Text("Enter time in \(inputUnit)")
+            }
+            
+            Section {
+                Text("Calculated time is \(convertedTimeIs, specifier: "%.2f") in \(outputUnit)")
+            }
+        }
+        .toolbar {
+            ToolbarItemGroup(placement: .keyboard) {
+                Spacer()
+                
+                Button("Done") {
+                    timeIsFocused = false
+                }
+            }
+        }
     }
 }
 
